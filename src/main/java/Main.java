067fff280b5ccc446306a9f95a55b4452df9c093
iratusWH <MarkelovAA@mpei.ru>
2022-04-05 11:09:13 +0300

    3 => 5.4.22-11:07: added new methods in class ToolBox: - dirList - extracting directories of project. - readFile - reading file by path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,41 +1,28 @@
-import com.github.javaparser.*;
-import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import support.classes.MethodNameCollector;
+import support.classes.ToolBox;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static support.classes.ToolBox.dirList;
+import static support.classes.ToolBox.readFile;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
 
-        CompilationUnit cu = StaticJavaParser.parse(
-                new File("/home/xela/IdeaProjects/diploma/src/main/java/Main.java")
+        List<String> paths = dirList("/home/xela/IdeaProjects/diploma/src/main/java")
+                .stream()
+                .map(File::toString)
+                .toList();
+
+        System.out.println(
+                paths
         );
 
-        Optional<ClassOrInterfaceDeclaration> declaration = cu.getClassByName("Main");
-        Optional<ClassOrInterfaceDeclaration> declaration1 = cu.getInterfaceByName("NewInterface");
-
-        System.out.println(declaration1);
-
-        List<String> collector = new ArrayList<>();
-        MethodNameCollector methodNameCollector = new MethodNameCollector();
-        //methodNameCollector.visit(declaration1.get(), collector);
-        methodNameCollector.visit(declaration.get(), collector);
-
-        System.out.println(collector);
-        System.out.println("Hello, world!");
-    }
-
-    public void getSomething(){
-
-    }
-
-    public void getSomething(String str){
-
+        System.out.println(
+                paths.stream().map(ToolBox::readFile).collect(Collectors.toList())
+        );
     }
 }
 
--- src/main/java/support/classes/ToolBox.java
+++ src/main/java/support/classes/ToolBox.java
@@ -1,4 +1,84 @@
 package support.classes;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
 public class ToolBox {
+
+    public static List<File> dirList(String startDirectory) {
+        List<File> javaFilesList = new ArrayList<>();
+        List<Path> directoryList = new ArrayList<>();
+        List<Path> directorySecondList = new ArrayList<>();
+
+        Path startDirectoryPath = Path.of(startDirectory);
+        File directoryFile;
+        boolean isNotEndOfDirectories = true;
+
+        try {
+            Files.newDirectoryStream(startDirectoryPath)
+                    .forEach(directoryList::add);
+        } catch (IOException ignored) {
+            System.out.println("ToolBox -> dirList(startDirectory): Error while getting directories");
+        }
+
+        int depth = 0;
+        while (isNotEndOfDirectories) {
+            for (Path filePath : directoryList) {
+
+                directoryFile = Optional.of(filePath)
+                        .map(Path::toFile)
+                        .orElse(null);
+
+                if (directoryFile.isFile()) {
+                    javaFilesList.add(directoryFile);
+                }
+
+                if (directoryFile.isDirectory()) {
+                    directorySecondList.addAll(
+                            Arrays.stream(
+                                    Objects.requireNonNull(
+                                            filePath.toFile()
+                                                    .listFiles())
+                                    )
+                                    .map(File::toPath)
+                                    .toList()
+                    );
+                }
+                System.out.println("-".repeat(3 * depth) + "| " + filePath);
+            }
+            directoryList.clear();
+            directoryList.addAll(directorySecondList);
+            isNotEndOfDirectories = !directoryList.isEmpty();
+            directorySecondList.clear();
+
+            ++depth;
+        }
+
+        return javaFilesList;
+    }
+
+    public static String readFile(String path){
+        File file = new File(path);
+        try {
+            BufferedReader br = new BufferedReader(
+                    new FileReader(file)
+            );
+
+            String st;
+            StringBuilder textFile = new StringBuilder();
+
+            while ((st = br.readLine()) != null)
+                textFile.append("\n").append(st);
+
+            return textFile.toString();
+        } catch (IOException ignored){
+            return "";
+        }
+    }
+
 }
